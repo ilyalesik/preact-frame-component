f93f5af7b582a359f025305a5d005888081cab45
Ilia Lesik <ilialesik@gmail.com>
2017-07-04 12:26:29 +0300

    preact

--- src/DocumentContext.jsx
+++ src/DocumentContext.jsx
@@ -1,17 +1,7 @@
+import { h } from 'preact';
 import React, { Component, Children } from 'react'; // eslint-disable-line no-unused-vars
-import PropTypes from 'prop-types';
 
 export default class DocumentContext extends Component {
-  static propTypes = {
-    document: PropTypes.object.isRequired,
-    window: PropTypes.object.isRequired,
-    children: PropTypes.element.isRequired
-  };
-
-  static childContextTypes = {
-    document: PropTypes.object.isRequired,
-    window: PropTypes.object.isRequired
-  };
 
   getChildContext() {
     return {
